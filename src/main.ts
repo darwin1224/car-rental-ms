820e9dba8485f8cedd163b1c38c6de843d520bad
darwin1224 <darwinblacks@gmail.com>
2020-07-12 14:52:54 +0700

    feat: add common security and compression

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { AppModule } from '@app/app.module';
 import { NestFactory } from '@nestjs/core';
+import compression from 'compression';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
+  app.use(compression());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 50,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
