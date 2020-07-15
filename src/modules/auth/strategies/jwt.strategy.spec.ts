cf83f2ec1004a0b32282f698ca72e5f1dc896eab
darwin1224 <darwinblacks@gmail.com>
2020-07-15 22:36:22 +0700

    test(unit): add jwt strategy test

--- src/modules/auth/strategies/jwt.strategy.spec.ts
+++ src/modules/auth/strategies/jwt.strategy.spec.ts
@@ -2,17 +2,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { JwtStrategy } from './jwt.strategy';
 
 describe('JwtStrategy', () => {
-  let service: JwtStrategy;
+  let jwtStrategy: JwtStrategy;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [JwtStrategy],
     }).compile();
 
-    service = module.get<JwtStrategy>(JwtStrategy);
+    jwtStrategy = module.get<JwtStrategy>(JwtStrategy);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('validate()', () => {
+    const expected = jwtStrategy.validate({
+      name: 'Administrator',
+      username: 'admin',
+      role: 'admin',
+    });
+
+    expect(typeof expected === 'object').toEqual(true);
+    expect(expected).toEqual({
+      name: 'Administrator',
+      username: 'admin',
+      role: 'admin',
+    });
   });
 });
