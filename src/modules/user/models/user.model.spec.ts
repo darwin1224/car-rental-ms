850d6000d3651b94925895cc13aba541efb4b803
darwin1224 <darwinblacks@gmail.com>
2020-07-15 22:33:45 +0700

    test(unit): add user model test

--- src/modules/user/models/user.model.spec.ts
+++ src/modules/user/models/user.model.spec.ts
@@ -1,7 +1,25 @@
 import { User } from './user.model';
 
+jest.mock('bcryptjs', () => ({
+  hashSync: jest.fn(password => `$1$${password}`),
+}));
+
 describe('User', () => {
+  let user: User;
+
+  beforeEach(() => {
+    user = new User();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
-    expect(new User()).toBeDefined();
+    expect(user).toBeDefined();
+  });
+
+  it('hashPassword()', () => {
+    expect(user.hashPassword()).toBeUndefined();
   });
 });
